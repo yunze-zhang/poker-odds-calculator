0b72b2005840c4184dec29b8c26f5267fac0dfef
Leo Zhang <yzha0592@uni.sydney.edu.au>
2021-02-22 18:15:40 +1100

    Added base of algorithm to determine winner

--- poker_odds.py
+++ poker_odds.py
@@ -1,13 +1,28 @@
 import random
+from enum import Enum
+
+class Hand(Enum):
+    HIGH_CARD = 0
+    ONE_PAIR = 1
+    TWO_PAIR = 2
+    THREE_OF_A_KIND = 3
+    STRAIGHT = 4
+    FLUSH = 5
+    FULL_HOUSE = 6
+    FOUR_OF_A_KIND = 7
+    STRIGHT_FLUSH = 8
 
 class PokerSimulator:
 
     def __init__(self, player_cards, board_state):
         self.player_cards = player_cards
-        self.board_state = board_state
+        if not board_state:
+            self.board_state = []
+        else:
+            self.board_state = board_state
+        self.rank_values = self.create_rank_dictionary()
         self.deck = self.generate_new_deck()
         self.remove_player_cards_from_deck()
-        self.shuffle_deck()
         
     def generate_new_deck(self):
         deck = []
@@ -15,6 +30,14 @@ class PokerSimulator:
             for rank in 'A23456789TJQK':
                 deck.append(rank+suit)
         return deck
+    
+    def create_rank_dictionary(self):
+        value = 2
+        rank_values = {}
+        for rank in '23456789TJQKA':
+            rank_values[rank] = value
+            value += 1
+        return rank_values
     
     def shuffle_deck(self):
         random.shuffle(self.deck)
@@ -28,8 +51,24 @@ class PokerSimulator:
                     else:
                         raise Exception("Invalid Player Card")
     
-    
-    
-    
+    def deal_card(self):
+        return self.deck.pop()
 
+    def simulate_full_board(self):
+        self.shuffle_deck()
+        while len(self.board_state) < 5:
+            next_card = self.deal_card()
+            self.board_state.append(next_card)
+    
+    def determine_winner(self):
+        for player_hand in self.player_cards:
+            board = self.board_state + player_hand
+            board_ranks = [card[0] for card in board]
+            board_suits = [card[1] for card in board]
+            hand_strength = []
+            
+    def check_high_card(self):
+        pass
 
+    def check_one_pair(self):
+        pass
